20aee11e480803bb39ee8fdfce95a70e0ddc19e5
eduvoltatone <108620108+eduvoltatone@users.noreply.github.com>
2025-03-20 22:08:32 -0300

    Update BookRepository.java

--- src/main/java/com/book/book/BookRepository.java
+++ src/main/java/com/book/book/BookRepository.java
@@ -1,7 +1,9 @@
 package com.book.book;
 
+//import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<BookModel, Long> {
-
+    //Optional<BookModel> findById(Long id); // Ensure this is here
 }
